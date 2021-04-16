c6008abfa9f9ac7ce45bc8727a8b7c24cc668b00
Celeo <mattboulanger@fastmail.com>
2021-04-15 20:23:04 -0700

    Not doing functional tests for side project

--- src/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { App } from "../src/App";
-import { mount } from "enzyme";
-
-describe("App.tsx", () => {
-  test("Mounts", () => {
-    mount(<App />);
-  });
-});
